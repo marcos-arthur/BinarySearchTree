04d26b9f06533569b619c7e38c47c9f018a0f49e
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2024-01-20 23:08:02 -0300

    implementação de remove

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -55,4 +55,56 @@ public class BinarySearchTree {
             return Search(node.getRight_child(), value);
         }
     }
+
+    void remove(int value){
+        remove(root, value);
+    }
+
+    private Node remove(Node node, int value){
+        if(node == null){
+            return node;
+        }
+
+        if(node.getValue() == value){
+            if (node.getRight_child() == null) {
+                return node.getLeft_child();
+            } else if (node.getLeft_child() == null) {
+                return node.getRight_child();
+            } else {
+
+                Node greaterLeftSideNodeParent = node;
+                Node greaterLeftSideNode = node.getLeft_child();
+                while (greaterLeftSideNode.getRight_child() != null){
+                    greaterLeftSideNodeParent = greaterLeftSideNode;
+                    greaterLeftSideNode = greaterLeftSideNode.getRight_child();
+                }
+
+                if(greaterLeftSideNodeParent == node){
+                    swap(node, greaterLeftSideNode);
+                    node.setLeft_child(greaterLeftSideNode.getLeft_child());
+                    greaterLeftSideNode.setLeft_child(null);
+                }else{
+                    swap(node, greaterLeftSideNode);
+                    greaterLeftSideNodeParent.setRight_child(null);
+                }
+
+                return node;
+            }
+        }else{
+            if(value < node.getValue()){
+                node.setLeft_child(remove(node.getLeft_child(), value));
+                return node;
+            }else {
+                node.setRight_child(remove(node.getRight_child(), value));
+                return node;
+            }
+        }
+    }
+
+    private void swap(Node n1, Node n2){
+        int aux = n1.getValue();
+
+        n1.setValue(n2.getValue());
+        n2.setValue(aux);
+    }
 }
